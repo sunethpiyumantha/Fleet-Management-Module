e79e32479bb74353a1a7e43106205e0d561797e5
Maleesha <devindihewage@gmail.com>
2025-06-11 13:48:53 +0530

    add seeder

--- database/seeders/VehicleCategorySeeder.php
+++ database/seeders/VehicleCategorySeeder.php
@@ -1,15 +1,24 @@
 <?php
+
 namespace Database\Seeders;
-use App\Models\VehicleCategory;
+
 use Illuminate\Database\Seeder;
+use App\Models\VehicleCategory;
+use App\Models\VehicleSubCategory;
+use App\Models\VehicleRequest;
 
 class VehicleCategorySeeder extends Seeder
 {
     public function run()
     {
-        $categories = ['Light Vehicle', 'Motor Vehicle', 'Heavy Vehicle', 'Off-Road Vehicle'];
-        foreach ($categories as $category) {
-            VehicleCategory::create(['category' => $category]);
-        }
+        $category1 = VehicleCategory::create(['category' => 'Car']);
+        $subCategory1 = VehicleSubCategory::create(['cat_id' => $category1->id, 'sub_category' => 'Sedan']);
+        VehicleRequest::create([
+            'cat_id' => $category1->id,
+            'sub_cat_id' => $subCategory1->id,
+            'qty' => 1,
+            'date_submit' => now(),
+            'status' => 'pending',
+        ]);
     }
 }
