35695d482d43ae0607f40dff0097ec9ca63f6aab
Maleesha <devindihewage@gmail.com>
2025-06-13 08:25:03 +0530

    engine capacity updated

--- app/Models/VehicleEngineCapacity.php
+++ app/Models/VehicleEngineCapacity.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VehicleEngineCapacity extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
     protected $table = 'vehicle_engine_capacities';
     protected $fillable = ['engine_capacity'];
 }
