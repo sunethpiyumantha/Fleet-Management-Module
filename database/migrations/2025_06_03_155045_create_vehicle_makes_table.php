0715dbe2ac3a2163c8688cc6ec1f8c05d1721631
Maleesha <devindihewage@gmail.com>
2025-06-13 08:31:39 +0530

    vehicle make updated

--- database/migrations/2025_06_03_155045_create_vehicle_makes_table.php
+++ database/migrations/2025_06_03_155045_create_vehicle_makes_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
             $table->id();
             $table->string('make')->unique();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
